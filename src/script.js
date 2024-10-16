253abcf4470add8b67397dbcc017906ff7846358
Jhonattan <jfaponte403@gmail.com>
2024-10-16 11:17:02 -0500

    Improve the UI/UX for the app

--- src/script.js
+++ src/script.js
@@ -1,44 +1,60 @@
-const formulario = document.getElementById("formulario")
-console.log(formulario)
+const enviarFormulario = async (event) => {
+    event.preventDefault();
 
-const enviarFormulario = (event) => {
-    event.preventDefault()
-
-    const datosFormulario = new FormData(event.target)
+    const datosFormulario = new FormData(event.target);
 
     const datosPost = {
         "frecuencia": datosFormulario.get("txtFrecuencia"),
         "potencia": datosFormulario.get("txtPotencia"),
         "altura": datosFormulario.get("txtAltura"),
-    }
+    };
 
-    console.log(datosPost)
+    try {
+        const response = await fetch("http://127.0.0.1:8000/datos", {
+            method: "POST",
+            body: JSON.stringify(datosPost),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-    fetch("http://127.0.0.1:8000/datos", {
-        method: "POST",
-        body: JSON.stringify(datosPost),
-        headers: {
-            'Content-Type': 'application/json'
+        if (response.status === 201) {
+            const datosDevueltos = await response.json();
+
+            const antennaInfo = document.getElementById("antennaInfo");
+            antennaInfo.innerHTML = `
+                <img alt="antena" src="src/img/ic_antena.svg" id="icono-antena" class="img-fluid" />
+                Frecuencia: ${datosPost.frecuencia} MHz, Potencia: ${datosPost.potencia} W, Altura: ${datosPost.altura} m
+            `;
+
+            const zonas = [
+                {
+                    id: "zonaExclusion",
+                    text: `La <span class="bold">Zona de Exclusión</span> está ubicada en un radio de <span class="bold">${datosDevueltos.zona_exclusion.toFixed(2)} metros</span> desde la base de la antena. En esta zona, está prohibido el acceso debido a los niveles elevados de radiación. Ninguna actividad se permite en este rango.`
+                },
+                {
+                    id: "zonaOcupacional",
+                    text: `La <span class="bold">Zona Ocupacional</span>, también conocida como zona de acceso restringido, se extiende desde el final de la Zona de Exclusión hasta un radio de <span class="bold">${datosDevueltos.zona_ocupacion.toFixed(2)} metros</span>. Solo el personal autorizado puede ingresar a esta área, debido a los niveles controlados de radiación.`
+                },
+                {
+                    id: "zonaPoblacional",
+                    text: `La <span class="bold">Zona Poblacional</span> comienza desde el final de la Zona Ocupacional y se extiende hasta un radio de <span class="bold">${datosDevueltos.zona_poblacion.toFixed(2)} metros</span>.`
+                }
+            ];
+
+
+            zonas.forEach(zona => {
+                const zonaDiv = document.getElementById(zona.id);
+                zonaDiv.style.display = "flex";
+                document.getElementById(`${zona.id}Text`).innerHTML = zona.text;
+            });
+
+        } else {
+            console.error(`Error: Status code ${response.status}`);
         }
-    })
-    .then(response => response.json())
-    .then(datosDevueltos => {
-        console.log(datosDevueltos)
+    } catch (error) {
+        console.error("Error en la petición:", error);
+    }
+};
 
-        const mostrarZonaExc = document.getElementById("zonaExclusion")
-        mostrarZonaExc.innerText = datosDevueltos.zona_exclusion
-        const mostrarZonaOcu = document.getElementById("zonaOcupacional")
-        mostrarZonaOcu.innerText = datosDevueltos.zona_ocupacion
-        const mostrarZonaPob = document.getElementById("zonaPoblacional")
-        mostrarZonaPob.innerText = datosDevueltos.zona_poblacion
-    })
-}
-
-formulario.addEventListener('submit', enviarFormulario)
-
-/*{
-    "txtSenal": "1415",
-    "txtFrecuencia": "154",
-    "txtPotencia": "245",
-    "txtAltura": "789"
-}*/
+formulario.addEventListener('submit', enviarFormulario);
